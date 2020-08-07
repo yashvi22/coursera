de087f8f5ca12065c7dbf3e8aeb26f473636f7cc
yashvi22 <66746662+yashvi22@users.noreply.github.com>
2020-08-07 10:33:18 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -114,7 +114,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-       var homeHtmlToInsertIntoMainPage = insertProperty(buildCategoriesViewHtml,
+       var homeHtmlToInsertIntoMainPage = insertProperty(buildAndShowMenuItemsHTML,
         "randomCategoryShortName",
         chosenCategoryShortName);
 
